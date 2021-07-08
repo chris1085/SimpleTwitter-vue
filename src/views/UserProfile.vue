e2db2071f8fb5ebe6e481215a26ba0b86674cc20
chris1085 <itscfchiang@gmail.com>
2021-07-08 17:12:23 +0800

    feat: Add userProfile layout

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -1,7 +1,37 @@
 <template>
-  <div></div>
+  <div>
+    <user-profile-info />
+    <user-profile-Nav
+      @changeNavPage="changeNavPage"
+      @changeNavPageTweeter="changeNavPage"
+    />
+    <user-profile-card :init-is-favorite="isFavorite" />
+  </div>
 </template>
 
 <script>
-export default {}
+import UserProfileInfo from '../components/UserProfileInfo.vue'
+import UserProfileNav from '../components/UserProfileNav.vue'
+import UserProfileCard from '../components/UserProfileCard.vue'
+
+export default {
+  name: 'UserProfile',
+  components: { UserProfileInfo, UserProfileNav, UserProfileCard },
+  data() {
+    return {
+      isFavorite: false
+    }
+  },
+  methods: {
+    changeNavPage(selected) {
+      if (selected === 'favorites') {
+        this.isFavorite = true
+      } else {
+        this.isFavorite = false
+      }
+    }
+  }
+}
 </script>
+
+<style lang="scss" scoped></style>
