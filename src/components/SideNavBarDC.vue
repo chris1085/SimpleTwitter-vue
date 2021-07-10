74e2a9f172e5aa37eeb0efa7c1a165fbcf40826a
chris1085 <itscfchiang@gmail.com>
2021-07-10 14:48:07 +0800

    feat: Add sideNavBar layout(double C version)

---
+++ src/components/SideNavBarDC.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="sideNaveBar d-flex flex-column justify-content-between">
+    <div class="sideNaveBar-menu-container">
+      <h1 class="logo">
+        <router-link class="logo-link" to="/main"
+          >AlphaCamp Simple Twitter</router-link
+        >
+      </h1>
+      <ul class="menuList">
+        <li class="list-item">
+          <router-link to="/main" class="list-link d-flex align-items-center">
+            <div class="icon-container-home"></div>
+            首頁</router-link
+          >
+        </li>
+        <li class="list-item">
+          <router-link
+            to="/user/:id"
+            class="list-link d-flex align-items-center"
+          >
+            <div class="icon-container-profile"></div>
+            個人資料</router-link
+          >
+        </li>
+        <li class="list-item">
+          <router-link
+            to="/setting"
+            class="list-link d-flex align-items-center"
+          >
+            <div class="icon-container-setting"></div>
+            設定</router-link
+          >
+        </li>
+      </ul>
+      <button class="btn btn-primary btn-nav-tweet">推文</button>
+    </div>
+
+    <div class="logout-container">
+      <router-link to="/logout" class="logout-link d-flex align-items-center">
+        <div class="icon-container-logout"></div>
+        登出</router-link
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SideNavBarDC'
+}
+</script>
