340b8ce9331479da7b164f78de92c89671e0365c
chris1085 <itscfchiang@gmail.com>
2021-07-10 19:29:36 +0800

    feat: Add sideNavBar and followingsCard in userFollower and userFollowings page

--- src/views/UserFollowers.vue
+++ src/views/UserFollowers.vue
@@ -1,43 +1,56 @@
 <template>
-  <div>
-    <header class="">
-      <div class="d-flex align-items-center">
-        <a href="#" class="leftArrow"><i class="fas fa-arrow-left"></i></a>
-        <div class="header-title">
-          <h3 class="font-bold">John Doe</h3>
-          <span class="subInfo">25推文</span>
-        </div>
-      </div>
-    </header>
-    <ul class="followMenu d-flex">
-      <li
-        class="followMenu-item"
-        :class="{ followActive: selected === 'followers' }"
-        @click="selected = 'followers'"
-      >
-        跟隨者
-      </li>
-      <li
-        class="followMenu-item"
-        :class="{ followActive: selected === 'followings' }"
-        @click="selected = 'followings'"
-      >
-        正在跟隨
-      </li>
-    </ul>
+  <div class="d-flex">
+    <SideNavBarDC />
 
-    <UserFollowersCard v-if="selected === 'followers'" />
-    <UserFollowingsCard v-if="selected === 'followings'" />
+    <div>
+      <header class="">
+        <div class="d-flex align-items-center">
+          <a href="#" class="leftArrow"><i class="fas fa-arrow-left"></i></a>
+          <div class="header-title">
+            <h3 class="font-bold">John Doe</h3>
+            <span class="subInfo">25推文</span>
+          </div>
+        </div>
+      </header>
+      <ul class="followMenu d-flex">
+        <li
+          class="followMenu-item"
+          :class="{ followActive: selected === 'followers' }"
+          @click="selected = 'followers'"
+        >
+          跟隨者
+        </li>
+        <li
+          class="followMenu-item"
+          :class="{ followActive: selected === 'followings' }"
+          @click="selected = 'followings'"
+        >
+          正在跟隨
+        </li>
+      </ul>
+
+      <UserFollowersCard v-if="selected === 'followers'" />
+      <UserFollowingsCard v-if="selected === 'followings'" />
+    </div>
+
+    <FollowingsCardDC />
   </div>
 </template>
 
 <script>
 import UserFollowersCard from '../components/UserFollowersCard.vue'
 import UserFollowingsCard from '../components/UserFollowingsCard.vue'
+import FollowingsCardDC from '../components/FollowingsCardDC.vue'
+import SideNavBarDC from '../components/SideNavBarDC.vue'
 
 export default {
   name: 'UserFollowers',
-  components: { UserFollowingsCard, UserFollowersCard },
+  components: {
+    UserFollowingsCard,
+    UserFollowersCard,
+    FollowingsCardDC,
+    SideNavBarDC
+  },
   data() {
     return {
       selected: 'followers'
